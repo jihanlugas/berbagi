0fcacee2f2eeb3adda847c29e977710c224aa4f2
jihanlugas <jihanlugas2@gmail.com>
2020-04-19 02:41:23 +0700

    Kontak

---
+++ database/seeds/KontakSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class KontakSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
